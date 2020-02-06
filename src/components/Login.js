5ed325d529ccac51456c0659d5b32bf43d07d3e5
HrithikMittal <adhikanshmittal@gmail.com>
2020-02-06 16:28:00 +0530

    Login into the system

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,27 +1,75 @@
-import React from "react";
-const Login = props => {
-  return (
-    <div className="container">
-      <form>
-        <div className="form-group">
-          <label>Registration Number</label>
-          <input type="text" className="form-control" id="exampleInputEmail1" />
-        </div>
-        <div className="form-group">
-          <label>Password</label>
-          <input
-            type="password"
-            className="form-control"
-            id="exampleInputPassword1"
-          />
-        </div>
+import React, { Component } from "react";
+import axios from "axios";
 
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </form>
-    </div>
-  );
-};
+class Login extends Component {
+  state = {
+    regno: "",
+    password: "",
+    token: "",
+    isSigned: false,
+    message: ""
+  };
+
+  onLogin = () => {
+    const data = {
+      regno: this.state.regno,
+      password: this.state.password
+    };
+    console.log("hey");
+    axios
+      .post("http://localhost:8080/user/signin", data)
+      .then(response => {
+        console.log(response);
+        this.setState({ token: response.token });
+        this.setState({ isSigned: true });
+        this.setState({ message: "You are successfully Login" });
+      })
+      .catch(err => {
+        console.log("Error message is ", err.message);
+      });
+  };
+
+  render() {
+    let form = "";
+    if (this.state.isSigned === false) {
+      form = (
+        <div>
+          <div className="form-group">
+            <label>Registration Number</label>
+            <input
+              type="text"
+              className="form-control"
+              id="regno"
+              value={this.state.regno}
+              onChange={event => this.setState({ regno: event.target.value })}
+            />
+          </div>
+          <div className="form-group">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              value={this.state.password}
+              onChange={event =>
+                this.setState({ password: event.target.value })
+              }
+            />
+          </div>
+
+          <button onClick={() => this.onLogin()} className="btn btn-primary">
+            Submit
+          </button>
+        </div>
+      );
+    }
+    return (
+      <div className="container">
+        <h3>{this.state.message}</h3>
+        {form}
+      </div>
+    );
+  }
+}
 
 export default Login;
